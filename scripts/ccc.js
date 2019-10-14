10f302cefc3cf42c1303e18563f60cfc02e90e0a
br0keh <abertoemail+8@gmail.com>
2019-10-14 02:59:45 -0300

    added card number validation

--- scripts/ccc.js
+++ scripts/ccc.js
@@ -8,12 +8,38 @@ function config(ConfigObject = Object) {
   Options.config = ConfigObject;
 }
 
+function validateCardNumber(number) {
+  var regex = new RegExp("^[0-9]{16}$");
+  if (!regex.test(number)) return false;
+
+  return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+  var sum = 0;
+  for (var i = 0; i < val.length; i++) {
+    var intVal = parseInt(val.substr(i, 1));
+    if (i % 2 == 0) {
+      intVal *= 2;
+      if (intVal > 9) {
+        intVal = 1 + (intVal % 10);
+      }
+    }
+    sum += intVal;
+  }
+  return sum % 10 == 0;
+}
+
 async function check(
   card = String,
   month = String,
   year = String,
   cvv = String
 ) {
+  if (!validateCardNumber(card)) {
+    return { error: "Invalid card number." };
+  }
+
   // Payment Request Options
   let pRequestOptions = Options.config;
 
